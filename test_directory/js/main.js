60f29c6b58faf3e2dbac233181001b266e2ae1c3
dongpangcheckm8 <dongpangcheckm8@gmail.com>
2019-09-19 20:39:26 +0900

    event delegation

--- test_directory/js/main.js
+++ test_directory/js/main.js
@@ -1,2 +1,12 @@
-var elHeading = document.querySelector("h1");
-foo(elHeading);
+var log = document.querySelector(".log");
+var ul = document.querySelector("ul");
+
+ul.addEventListener("click", function(event){
+  // target이 image이면 src를 추출해서 출력
+  var target = event.target;
+  if(target.tagName === "IMG") {
+    log.innerHTML = "IMG URL IS " + target.src;
+  } else if(target.tagName === "LI") {
+    log.innerHTML = "IMG URL IS " + target.firstElementChild.src;
+  }
+});
